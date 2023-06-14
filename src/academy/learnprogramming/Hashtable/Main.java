d94186148185fc511810041101743c6a6d65912e
Tootin0730 <yisan1004@gmail.com>
2023-06-14 13:05:34 +0900

    Linear Probing

--- src/academy/learnprogramming/Hashtable/Main.java
+++ src/academy/learnprogramming/Hashtable/Main.java
@@ -1,22 +1,29 @@
 package academy.learnprogramming.Hashtable;
 
 public class Main {
+
     public static void main(String[] args) {
         Employee janeJones = new Employee("Jane", "Jones", 123);
         Employee johnDoe = new Employee("John", "Doe", 4567);
         Employee marySmith = new Employee("Mary", "Smith", 22);
         Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
-        Employee billEnd = new Employee("Bill", "End", 79);
+        Employee billEnd = new Employee("Bill", "End", 78);
 
         SimpleHashtable ht = new SimpleHashtable();
         ht.put("Jones", janeJones);
         ht.put("Doe", johnDoe);
-        ht.put("Smith", marySmith);
         ht.put("Wilson", mikeWilson);
+        ht.put("Smith", marySmith);
 
-        //ht.printHashtable();
+        ht.printHashtable();
 
-        System.out.println("Retrieve key Wilson " + ht.get("Wilson"));
+        System.out.println("Retrieve key Wilson: " + ht.get("Wilson"));
+        System.out.println("Retrieve key Smith: " + ht.get("Smith"));
 
+        ht.remove("Wilson");
+        ht.remove("Jones");
+        ht.printHashtable();
+
+        System.out.println("Retrieve key Smith: " + ht.get("Smith"));
     }
 }
