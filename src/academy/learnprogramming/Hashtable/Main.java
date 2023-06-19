d9fe5ce52bed28db58c1d2dc3b5e4082a5488582
Tootin0730 <yisan1004@gmail.com>
2023-06-19 18:46:27 +0900

    Bucket Sort implementation

--- src/academy/learnprogramming/Hashtable/Main.java
+++ src/academy/learnprogramming/Hashtable/Main.java
@@ -1,29 +1,39 @@
 package academy.learnprogramming.Hashtable;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Employee janeJones = new Employee("Jane", "Jones", 123);
-        Employee johnDoe = new Employee("John", "Doe", 4567);
-        Employee marySmith = new Employee("Mary", "Smith", 22);
-        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+        int[] intArray = { 54, 46, 83, 66, 95, 92, 43 };
 
-        Map<String, Employee> hashMap = new HashMap<String, Employee>();
-        hashMap.put("Jones", janeJones);
-        hashMap.put("Doe", johnDoe);
-        hashMap.put("Smith", marySmith);
-        Employee employee = hashMap.putIfAbsent("Doe", mikeWilson);
-        System.out.println(employee);
+        bucketSort(intArray);
 
-        System.out.println(hashMap.getOrDefault("someone", mikeWilson));
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
 
-        System.out.println(hashMap.remove("Jones"));
-
-        hashMap.forEach((k, v) -> System.out.println("Key = " + k + ", Employee = " + v));
-
+    public static void bucketSort(int[] input) {
+        List<Integer>[] buckets = new List[10];
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i] = new ArrayList<Integer>();
+        }
+        for (int i = 0; i < input.length; i++) {
+            buckets[hash(input[i])].add(input[i]);
+        }
+        for (List bucket: buckets) {
+            Collections.sort(bucket);
+        }
+        int j = 0;
+        for (int i = 0; i < buckets.length; i++) {
+            for (int value: buckets[i]) {
+                input[j++] = value;
+            }
+        }
+    }
+    private static int hash(int value) {
+        return value / 10;
     }
 }
 
