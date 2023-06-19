f8f84a9c0d6f0406216e21c1c5ee30125e77043b
Tootin0730 <yisan1004@gmail.com>
2023-06-19 16:51:48 +0900

    Hashtable and the JDK

--- src/academy/learnprogramming/Hashtable/Employee.java
+++ src/academy/learnprogramming/Hashtable/Employee.java
@@ -11,12 +11,29 @@ public class Employee {
         this.id = id;
     }
 
-    public String getFirstName() {return firstName;}
-    public void setFirstName(String firstName) {this.firstName = firstName;}
-    public String getLastName() {return lastName;}
-    public void setLastName(String lastName) {this.lastName = lastName;}
-    public int getId() {return id;}
-    public void setId(int id) {this.id = id;}
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/academy/learnprogramming/Hashtable/Main.java
+++ src/academy/learnprogramming/Hashtable/Main.java
@@ -1,5 +1,8 @@
 package academy.learnprogramming.Hashtable;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,19 +11,18 @@ public class Main {
         Employee marySmith = new Employee("Mary", "Smith", 22);
         Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
 
-        ChainedHashtable ht = new ChainedHashtable();
-        ht.put("Jones", janeJones);
-        ht.put("Doe", johnDoe);
-        ht.put("Wilson", mikeWilson);
-        ht.put("Smith", marySmith);
+        Map<String, Employee> hashMap = new HashMap<String, Employee>();
+        hashMap.put("Jones", janeJones);
+        hashMap.put("Doe", johnDoe);
+        hashMap.put("Smith", marySmith);
+        Employee employee = hashMap.putIfAbsent("Doe", mikeWilson);
+        System.out.println(employee);
 
-        ht.printHashtable();
+        System.out.println(hashMap.getOrDefault("someone", mikeWilson));
 
-//        System.out.println("Retrieve key Smith: " + ht.get("Smith"));
-//
-//        ht.remove("Wilson");
-//        ht.remove("Jones");
-//        ht.printHashtable();
+        System.out.println(hashMap.remove("Jones"));
+
+        hashMap.forEach((k, v) -> System.out.println("Key = " + k + ", Employee = " + v));
 
     }
 }
